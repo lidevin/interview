7c6b8d407440a31e444aba9dbca57a31bbede1fe
Wcz <499960904@qq.com>
2018-05-02 19:00:44 +0800

    Revert "Merge branch 'master' of https://github.com/lidevin/interview"
    This reverts commit 0962414d906ce215cfe972f1c32ef6f8e455d7c9, reversing
    changes made to c4092b09f13571bf02c02ff2a3c46d7e59875ce2.

--- src/main/java/cn/lwy/service/CandidateService.java
+++ src/main/java/cn/lwy/service/CandidateService.java
@@ -1,9 +1,6 @@
 package cn.lwy.service;
 
-import java.util.List;
-
 import cn.lwy.pojo.Candidate;
-import cn.lwy.pojo.Paper;
 
 /**
  * 候选人的业务类
@@ -12,14 +9,9 @@ public interface CandidateService extends BaseService<Candidate>{
 	
 	/**
 	 * 根据用户名获取候选人(登录业务)
-	 * @param candidate  
+	 * @param candidate
 	 * @return
 	 */
-	boolean getByOpenid(Candidate candidate);
+	boolean getByName(Candidate candidate);
 	
-	/**
-	 * 获取最近考试的试卷
-	 * @param candidate  单位秒
-	 */
-	List<Paper> getRecentPaper(Long time);
 }
